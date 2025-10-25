cecec4b0894504fb2a126d61d7138c61f109767a
Kepka Ludovic <shionn@gmail.com>
2025-10-25 20:59:18 +0200

    nouveau tri

--- src/main/java/collections/db/dbo/SortBy.java
+++ src/main/java/collections/db/dbo/SortBy.java
@@ -11,13 +11,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SortBy {
 
-	name((a, b) -> a.getName().compareTo(b.getName())), number((a, b) -> {
+	edition((a, b) -> a.getEdition().compareTo(b.getEdition())),
+	name((a, b) -> a.getName().compareTo(b.getName())),
+	number((a, b) -> {
 		try {
 			NumberFormat format = NumberFormat.getIntegerInstance();
 			return Integer.compare(format.parse(a.getNumbers()).intValue(), format.parse(b.getNumbers()).intValue());
 		} catch (ParseException e) {
 			return 0;
 		}
+	}),
+	edition_number((a, b) -> {
+		int c = edition.comparator.compare(a, b);
+		return c == 0 ? number.comparator.compare(a, b) : c;
 	});
 
 	private final Comparator<Item> comparator;
